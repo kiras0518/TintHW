94047debe561ba005a7c37546f3451353ee82e1f
ting <kiras0518@gmail.com>
2024-03-30 14:17:38 +0800

    Initial Commit

---
+++ TintHW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TintHW
+//
+//  Created by Ting on 2024/3/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
